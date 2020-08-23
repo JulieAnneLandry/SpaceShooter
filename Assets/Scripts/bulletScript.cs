672715c9085330ff268bb08443981e0165428743
Julie-Anne Landry <julieannelandry94@gmail.com>
2020-08-22 22:19:16 -0400

    First commit

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // FixedUpdate is called at a fixed interval and is independant of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(0, speed);
+        rb2d.AddForce(rb2d.velocity, ForceMode2D.Force);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/spaceshipScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceshipScript : MonoBehaviour
+{
+    public float speed;
+    public float shootForce;
+    private Rigidbody2D rb2d;
+    private Vector2 inputs = Vector2.zero;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputs.x = Input.GetAxis("Horizontal");
+        inputs.y = Input.GetAxis("Vertical");
+    }
+
+    // FixedUpdate is called at a fixed interval and is independant of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + inputs * speed * Time.fixedDeltaTime);
+    }
+}
